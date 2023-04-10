e89055c1ab84bc64d0827bf2d25f0032047e6e92
mina eid <minaeid6@gmail.com>
2023-04-10 14:57:57 +0200

    Initial Commit

---
+++ Onboarding Screen/Onboarding_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_ScreenApp.swift
+//  Onboarding Screen
+//
+//  Created by Mina on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
