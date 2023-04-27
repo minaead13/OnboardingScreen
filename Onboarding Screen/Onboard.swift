ad955ae232ca8cfb6a53787c4f16d1b534614637
mina eid <minaeid6@gmail.com>
2023-04-28 02:20:27 +0300

    Edit

---
+++ Onboarding Screen/Onboard.swift
@@ -0,0 +1,8 @@
+//
+//  Onboard.swift
+//  Onboarding Screen
+//
+//  Created by Mina on 10/04/2023.
+//
+
+import Foundation
---
+++ Onboarding Screen/Preview Content/extensions.swift
@@ -0,0 +1,8 @@
+//
+//  extensions.swift
+//  Onboarding Screen
+//
+//  Created by Mina on 10/04/2023.
+//
+
+import Foundation
