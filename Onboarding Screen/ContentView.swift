6450057d7e833acd0da027f5849adddde5e1ef49
mina eid <minaeid6@gmail.com>
2023-04-28 02:20:43 +0300

    Add

--- Onboarding Screen/ContentView.swift
+++ Onboarding Screen/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var index = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                ForEach(0 ..< onboardItems.count , id: \.self) { i in
+                    Text(onboardItems[i].tag)
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal)
+                        .foregroundColor(.white)
+                        .background(onboardItems[i].color)
+                    .cornerRadius(8)
+                    .opacity(i == index ? 1 : 0)
+                    .scaleEffect(i == index ? 1 : 0)
+                    .animation(.easeInOut, value: i == index)
+                }
+            }
+                    TabView(selection: $index) {
+                        ForEach(0 ..< 3){ i in
+                            Image("Walkthrough_\(i)")
+                                .resizable()
+                                .padding()
+                                .aspectRatio(1 ,contentMode: .fit)
+                        }
+                    }
+            
+            .tabViewStyle(PageTabViewStyle())
+            
+            HStack(spacing: 4) {
+                ForEach(0 ..< 3){ i in
+                    Color(.blue)
+                        .opacity(i == index ? 1 : 0.5)
+                        .frame(width:i == index ? 8 : 16,height: 8)
+                        .animation(.easeInOut(duration: 0.4), value: i == index)
+                }
+            }
+            ZStack {
+                ForEach(0 ..< 3 ) { i in
+                    VStack {
+                        Text(onboardItems[i].title)
+                            .font(.largeTitle)
+                        Text(onboardItems[i].description)
+                            .foregroundColor(Color(.gray))
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 8)
+                            .padding(.bottom)
+                    }
+                    .opacity(i == index ? 1 : 0)
+                    .offset(CGSize(width: 0, height: i == index ? 0 : 100))
+                    .animation(.easeInOut, value:i == index)
+                }
+            }
+        }
+        Button {
+            
+        } label: {
+            Text("Get Started")
+                .font(.headline)
+                .frame(maxWidth: .infinity, maxHeight: 48)
+                .foregroundColor(.white)
+                .background(Color(.blue))
+                .cornerRadius(12)
         }
         .padding()
+        
     }
 }
 
--- Onboarding Screen/Onboard.swift
+++ Onboarding Screen/Onboard.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Onboard: Identifiable {
+    let id : UUID
+    let title: String
+    let description: String
+    let tag: String
+    let color: Color
+}
+
+var onboardItems: [Onboard] = [ Onboard(id: UUID(), title: "Vagabond", description: "we are shoemakers t heart and soul. We believe that great design does not have to cost a fortune", tag: "Footwear", color: Color(hex: 0xBBE583)) , Onboard(id: UUID(), title: "Sweatshirt", description: "The comfortable sweatshirt with a large front logo print.", tag: "Clothing", color: Color(hex: 0xFFC58B)), Onboard(id: UUID(), title: "Blouver", description: "Women blouvers. We are shoeamakers at heart and soul.", tag: "Newset", color: Color(hex: 0xB4E0FA))]
